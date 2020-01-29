6bf86be788bbe32849b38e1b088db0faaf1c8800
pgiannakopoulos <pgiannakopoulos397@gmail.com>
2020-01-29 23:35:10 +0200

    stats for brute

--- Taxi-v3_main.py
+++ Taxi-v3_main.py
@@ -11,35 +11,37 @@ from deep_q_learning import DQN_Agent
 from brute_algorithm import brute_agent
 
 env = gym.make("Taxi-v3")
-
-q_agent = Agent_QL(env)
 sim_episodes = 100
 
+brute_avg_timesteps, brute_avg_reward = brute_agent(env,sim_episodes)
+
+q_agent = Agent_QL(env)
 q_agent.train_agent(alpha = 0.4, gamma = 0.95, epsilon = 0.9, episodes = 10000,filename='training_ql.npy')
-q_avg_timesteps, q_avg_penalties = q_agent.simulate('training_ql.npy', visualize = False, episodes=sim_episodes)
+q_avg_timesteps, q_avg_reward  = q_agent.simulate('training_ql.npy', visualize = False, episodes=sim_episodes)
 
 sarsa_agent = Agent_Sarsa(env)
 sarsa_agent.train_agent(alpha = 0.4, gamma = 0.999, epsilon = 0.9, episodes = 10000, max_steps = 2500, n_tests = 20,filename='training_sarsa.npy')
-s_avg_timesteps, s_avg_penalties = sarsa_agent.simulate(filename = 'training_sarsa.npy', visualize = False, episodes=sim_episodes)
+s_avg_timesteps, s_avg_reward  = sarsa_agent.simulate(filename = 'training_sarsa.npy', visualize = False, episodes=sim_episodes)
 
 dqn_agent = DQN_Agent(env)
 dqn_agent.train_agent('dqn_weights.h5f')
-s_avg_timesteps,s_avg_reward = dqn_agent.simulate(episodes=sim_episodes, visualze = False)
+s_avg_timesteps, s_avg_reward = dqn_agent.simulate(episodes=sim_episodes, visualze = False)
 
 print(f"Results for {sim_episodes} episodes:")
 
+print("NO learning")
+print(f"Average timesteps per episode: {brute_avg_timesteps}")
+print(f"Average reward per episode: {brute_avg_reward}")
+
 print("Q learning")
 print(f"Average timesteps per episode: {q_avg_timesteps}")
-print(f"Average reward per episode: {q_avg_penalties}")
+print(f"Average reward per episode: {q_avg_reward }")
 
 print("Sarsa learning")
 print(f"Average timesteps per episode: {s_avg_timesteps}")
-print(f"Average reward per episode: {s_avg_penalties}")
+print(f"Average reward per episode: {s_avg_reward }")
 
 print("DQN learning")
 print(f"Average timesteps per episode: {s_avg_timesteps}")
 print(f"Average reward per episode: {s_avg_reward}")
-
-
-# brute_agent = brute_agent(env)
 
--- brute_algorithm.py
+++ brute_algorithm.py
@@ -1,33 +1,43 @@
 import gym
 
-def brute_agent(env):
+def brute_agent(env, episodes):
     env.reset()
+    total_epochs, total_penalties ,total_rewards = 0, 0, 0
 
     epochs = 0
     penalties, reward = 0, 0
 
     frames = [] # for animation
+    for _ in range(episodes):
+        done = False
+        env.reset()
+        epochs, penalties, reward = 0, 0, 0
+        while not done:
+            action = env.action_space.sample()
+            state, reward, done, info = env.step(action)
 
-    done = False
+            total_rewards+=reward
 
-    while not done:
-        action = env.action_space.sample()
-        state, reward, done, info = env.step(action)
+            if reward == -10:
+                penalties += 1
+            
+            # Put each rendered frame into dict for animation
+            frames.append({
+                'frame': env.render(mode='ansi'),
+                'state': state,
+                'action': action,
+                'reward': reward
+                }
+            )
 
-        if reward == -10:
-            penalties += 1
+            epochs += 1
+
+            total_penalties += penalties
+            total_epochs += epochs
         
-        # Put each rendered frame into dict for animation
-        frames.append({
-            'frame': env.render(mode='ansi'),
-            'state': state,
-            'action': action,
-            'reward': reward
-            }
-        )
+        
+    avg_timesteps = total_epochs / episodes
+    avg_penalties = total_penalties / episodes
+    avg_rewards = total_rewards / episodes
 
-        epochs += 1
-        
-        
-    print("Timesteps taken: {}".format(epochs))
-    print("Penalties incurred: {}".format(penalties))
+    return avg_timesteps, avg_rewards
